3a10ae467056391dfc436d0133c9042b968bcb12
mxding <937652174@qq.com>
2020-11-09 17:47:24 +0800

    test 1

--- src/main/java/net/ruixin/oauth/controller/MainController.java
+++ src/main/java/net/ruixin/oauth/controller/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     /**
-     *
+     * 测试
      * @return
      */
     @RequestMapping({"/", "/hello"})
